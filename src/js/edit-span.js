3676b9b6d7f94cae898e106a288e368e67a0eed7
wade <352912788@qq.com>
2018-06-22 10:39:22 +0800

    编辑，添加内容

--- src/js/edit-span.js
+++ src/js/edit-span.js
@@ -5,8 +5,7 @@
             <span class="editSpan">
                 <span v-show='!editing'>{{value}}</span>
                 <input v-show='editing' type="text" v-model='value' @input='triggleEdit'>
-                <button v-show='!editing' v-on:click = 'editing = !editing'>编辑</button>
-                <button v-show='editing' v-on:click = 'editing = !editing'>保存</button>
+                <button v-on:click = 'editing = !editing'>编辑</button>
             </span>
         `,
         data() {
--- src/js/newVue.js
+++ src/js/newVue.js
@@ -1,3 +1,5 @@
+
+
 {
 
 
@@ -5,12 +7,14 @@
         el: '#app',
         data: {
             editingName: false,
-            loginpage:false,
-            signUppage:false,
-            signInpage:true,
-            currentUser:{
-                id:'',
-                email:''
+            loginpage: false,
+            signUppage: false,
+            signInpage: true,
+            sharePage: false,
+            shareLink:'',
+            currentUser: {
+                objectId: '',
+                email: ''
             },
             resume: {
                 name: '宋萌',
@@ -18,33 +22,53 @@
                 birthday: '1995/12/06',
                 jobTitle: '初级前端工程师',
                 age: '23',
-                phone: '15172545344'
+                phone: '15172545344',
+                skills: [
+                    { name: '请添加技能', description: '请添加内容' },
+                    { name: '请添加技能', description: '请添加内容' },
+                    { name: '请添加技能', description: '请添加内容' },
+                    { name: '请添加技能', description: '请添加内容' }
+                ],
+                project:[
+                    {name:'项目名称',url:'www.xxx.com',keyword:'关键字',content:'具体内容'},
+                    {name:'项目名称',url:'www.xxx.com',keyword:'关键字',content:'具体内容'},
+                    {name:'项目名称',url:'www.xxx.com',keyword:'关键字',content:'具体内容'}    
+                ]
             },
-            signUp:{
-                email:'',
-                password:''
+            signUp: {
+                email: '',
+                password: ''
             },
-            loginIn:{
-                email:'',
-                password:''
+            loginIn: {
+                email: '',
+                password: ''
             }
-         
-
         },
         methods: {
-            onEdit(key,value) {
-                this.resume[key] = value
+            onEdit(key, value) {
+                let arr = key.split('.')
+                let result = this.resume
+                for (let i = 0; i < arr.length; i++) {
+                    if (i === arr.length - 1) {
+                        result[arr[i]] = value
+                    } else {
+                        result = result[arr[i]]
+                    }
+                }
             },
-            onClickSave(){ //保存
+            onClickSave() { //点击判断是否保存
                 let currentUser = AV.User.current()
-                if(!currentUser){
+                if (!currentUser) {
                     this.loginpage = true
-                
-                }else{
+
+                } else {
                     this.saveResume()
                 }
             },
-            onSignUp(){//注册
+            onClickShare() {
+                this.sharePage = true
+            },
+            onSignUp() {//注册
                 const user = new AV.User()
                 //设置用户名
                 user.setUsername(this.signUp.email)
@@ -52,60 +76,86 @@
                 user.setPassword(this.signUp.password)
                 //设置email
                 user.setEmail(this.signUp.email)
-                user.signUp().then(function(user){
-                    console.log(user)
-                },function(error){
+                user.signUp().then((user) => {
+                    alert('注册成功')
+                    user = user.toJSON()
+                    this.loginpage = false
+                    this.currentUser = {
+                        objectId: user.objectId,
+                        email: user.email
+                    }
+                    window.location.reload()
+                }, function (error) {
                     console.log(error)
                 })
             },
-            outLoginIn(){ //注销
+            outLoginIn() { //注销
                 AV.User.logOut();
                 // 现在的 currentUser 是 null 了
                 alert('注销成功')
                 window.location.reload()
 
             },
-            onLoginIn(){ //登录
-                AV.User.logIn(this.loginIn.email, this.loginIn.password).then( (user)=>{
+            onLoginIn() { //登录
+                AV.User.logIn(this.loginIn.email, this.loginIn.password).then((user) => {
                     user = user.toJSON()
                     alert('登录成功')
                     this.loginpage = false
                     this.currentUser = {
-                        id:user.objectId,
-                        email:user.email
+                        objectId: user.objectId,
+                        email: user.email
                     }
-                  }, function (error) {
-                        switch(error.code){
-                            case 211:
+                    window.location.reload()
+                }, function (error) {
+                    switch (error.code) {
+                        case 211:
                             alert('邮箱输入错误')
                             break
-                            case 210:
+                        case 210:
                             alert('邮箱与密码不匹配')
-                        }
-                  })
+                    }
+                })
             },
-            saveResume(){
-                let id = AV.User.current().id
-                var resume = AV.Object.createWithoutData('User',id);
+            saveResume() {//保存
+                let objectId = AV.User.current().toJSON().objectId
+                var resume = AV.Object.createWithoutData('User', objectId);
                 // 修改属性
-                resume.set('reusme', this.resume);
+                resume.set('resume', this.resume);  
                 // 保存到云端
-                resume.save();
+                resume.save()
+                alert('保存成功')
             },
-            getResume(){
+            getResume() {//获取数据
                 var query = new AV.Query('User');
-                query.get(this.currentUser.id).then(function (user) {
-                    console.log(id)
-                }, function (error) {
+                query.get(this.currentUser.objectId).then((user) => {
+                    user = user.toJSON()
+                    Object.assign(this.resume, user.resume)
+                }, (error) => {
                     console.log(error)
                 });
+            },
+            addSkill(){//添加技能树
+                this.resume.skills.push({ name: '请添加技能', description: '请添加技能描述' })
+            },
+            deleteSkill(index){
+                this.resume.skills.splice(index,1)
+            },
+            addProject(){
+                this.resume.project.push({name:'项目名称',url:'www.xxx.com',keyword:'关键字',content:'具体内容'} )
+            },
+            deleteProject(index){
+                this.resume.project.splice(index,1)
             }
         }
     })
 
     let currentUser = AV.User.current()
-    if(currentUser){
-        app.currentUser = currentUser
+    if (currentUser) {
+        app.currentUser = currentUser.toJSON()
+        console.log(location.href)
+        app.shareLink = location.href + '?user_id='+ app.currentUser.objectId
+        console.log(app.shareLink)
+        app.getResume()
     }
 
 }
